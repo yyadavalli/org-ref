e303ec8e862a37c1a343b472f9fccd8620b9189b
Jonathan Gregory <jgrg@autistici.org>
2018-05-03 12:45:40 -0300

    Downcase lowercase words
    Namely any word in org-ref-lower-case-words

--- org-ref-bibtex.el
+++ org-ref-bibtex.el
@@ -638,7 +638,7 @@ and books."
                       ;; are the first word
                       ((-contains? org-ref-lower-case-words
                                    (s-downcase word))
-                       word)
+                       (s-downcase word))
                       ;; Words that are quoted
                       ((s-starts-with? "\"" word)
                        (concat "\"" (s-capitalize (substring word 1))))
